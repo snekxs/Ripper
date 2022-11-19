e386b6d0428747980b9a08e8babeb577efc44a7b
Snekxs <snekxs@gmail.com>
2022-11-18 20:02:04 -0700

    Add Side Bar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+        <header className="AppTitle">
+            <h1>Ripper</h1>
+            <h1 className={"AppDescription"}>A simple React app</h1>
+        </header>
+    </div>
+  )
+}
+
+export default App
